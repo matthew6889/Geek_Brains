df0568f27d8d0af0f0da9b0bd648da246bf46bfc
Matvey <matveypashnev6889@gmail.com>
2023-09-07 22:11:50 +0400

    JS

---
+++ Знакомство с веб-технологиями/seminar2/seminar/JavaScript.js
@@ -0,0 +1,38 @@
+// let number_one = parseInt(prompt("Введите 1-ое число:"));
+// let number_two = parseInt(prompt("Введите 2-ое число:"));
+// alert(`${number_one} + ${number_two} = ${number_one + number_two}`);
+//``-буква Ё
+
+// let names = prompt("Введите своё имя:", "Имя");
+// alert(`Привет, ${names}!`);
+
+// function showName(names='Иван'){
+//     alert(`Тебя зовут ${names}`);
+// }
+
+// let test = prompt("Введите свое имя: ", "name");
+// if (test == '')
+//     showName();
+// else
+//     showName(test);
+
+// if (confirm("Вы уверены?"))
+//     alert("Мы рады, что вы уверены!");
+// else
+//     alert("Жаль, что вы не уверены");
+
+// let age = parseInt(prompt("Введите ваш возраст:"));
+
+// switch (age) {
+//   case 18:
+//     alert( 'Вы совершеннолетний, вам все можно' );
+//     break;
+//   case 10:
+//     alert( 'Вам надо учить уроки!' );
+//     break;
+//   case 30:
+//     alert( 'Ложитесь спать, завтра на работу' );
+//     break;
+//   default:
+//     alert( "Мы не знаем что Вам делать" );
+// }
