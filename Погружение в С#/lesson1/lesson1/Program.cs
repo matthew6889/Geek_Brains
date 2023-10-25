59ca1f64b3c4824f911d58bda8fd030d2e191b68
Matvey <matveypashnev6889@gmail.com>
2023-10-25 13:26:28 +0400

    первый семинар по погружению в С#

---
+++ Погружение в С#/lesson1/lesson1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace seminar1
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            int min = Convert.ToInt32(args[0]);
+            int max = Convert.ToInt32(args[0]);
+            for (int i = 0; i < args.Length; i++)
+            {
+                int M = Convert.ToInt32(args[i]);
+                if (M < min)
+                {
+                    min = M;
+                }
+                if (M > max)
+                {
+                    max = M;
+                }
+
+            }
+            Console.WriteLine($"[максимум -> {max} минимум -> {min}");
+        }
+    }
+}
