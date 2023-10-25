6783e374f3766cbfe2e2ae1e6eac857431dfed83
Matvey <matveypashnev6889@gmail.com>
2023-10-25 15:28:26 +0400

    дз по семинару 1

---
+++ Погружение в С#/home_work_lesson1/home_work_lesson1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace seminar1_home_work //Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b, введенные из командной строки, и выводящую результат выполнения на экран.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Не верное число аргументов!");
+            }
+            else
+            {
+                int a = Convert.ToInt32(args[0]);
+                char sign = Convert.ToChar(args[1]);
+                int b = Convert.ToInt32(args[2]);
+                int equals = 0;
+                if (sign == '+')
+                {
+                    equals = a + b;
+                }
+                if (sign == '-')
+                {
+                    equals = a - b;
+                }
+                if (sign == '*')
+                {
+                    equals = a * b;
+                }
+                if (sign == '/')
+                {
+                    equals = a / b;
+                }
+                Console.WriteLine($"{a}{sign}{b}={equals}");
+            }
+            
+        }
+    }
+}
