ed0729ef5002765982df9a0751d5866600c66311
Matvey <matveypashnev6889@gmail.com>
2023-12-03 15:21:43 +0400

    Семинар 2 и дз к нему

---
+++ Разработка приложения на C#/lesson3/Home_work/Home_work/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+Stack<Tuple<int, int>> _path = new Stack<Tuple<int, int>>();
+
+int[,] labirynth1 = new int[,]
+{
+{1, 1, 1, 1, 1, 1, 1 },
+{1, 1, 0, 0, 0, 0, 1 },
+{1, 1, 1, 1, 1, 0, 1 },
+{2, 0, 0, 0, 1, 0, 2 },
+{1, 1, 0, 2, 1, 1, 1 },
+{1, 1, 1, 1, 1, 1, 1 },
+{1, 1, 1, 2, 1, 1, 1 }
+};
+Console.WriteLine($"Количество выходов: {FindPath(1, 2)}");
+Console.WriteLine($"Количество выходов: {FindPath(3, 2)}");
+Console.WriteLine($"Количество выходов: {FindPath(1, 1)}");
+
+int FindPath(int i, int j)
+{
+    //Console.WriteLine(labirynth1[i, j]);
+    if (labirynth1[i, j] == 0) _path.Push(new(i, j));
+    
+    int exit = 0;
+
+    while (_path.Count > 0)
+    {
+        var current = _path.Pop();
+
+        if (labirynth1[current.Item1, current.Item2] == 2)
+        {
+            exit++;
+        }
+
+        labirynth1[current.Item1, current.Item2] = 1;
+
+        if (current.Item1 + 1 < labirynth1.GetLength(0)
+           && labirynth1[current.Item1 + 1, current.Item2] != 1)
+            _path.Push(new(current.Item1 + 1, current.Item2));
+
+        if (current.Item2 + 1 < labirynth1.GetLength(1) &&
+           labirynth1[current.Item1, current.Item2 + 1] != 1)
+            _path.Push(new(current.Item1, current.Item2 + 1));
+
+        if (current.Item1 > 0 && labirynth1[current.Item1 - 1, current.Item2] != 1)
+            _path.Push(new(current.Item1 - 1, current.Item2));
+
+        if (current.Item2 > 0 && labirynth1[current.Item1, current.Item2 - 1] != 1)
+            _path.Push(new(current.Item1, current.Item2 - 1));
+    }
+
+    return exit;
+}
