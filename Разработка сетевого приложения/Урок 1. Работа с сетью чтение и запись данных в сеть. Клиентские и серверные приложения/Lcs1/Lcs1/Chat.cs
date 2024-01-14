f2c219d8743db00b1278389a5095cd030bfec490
Matvey <matveypashnev6889@gmail.com>
2024-01-14 17:34:35 +0400

    семинар 1 с дз

---
+++ Разработка сетевого приложения/Урок 1. Работа с сетью чтение и запись данных в сеть. Клиентские и серверные приложения/Lcs1/Lcs1/Chat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lcs1
+{
+    internal class Chat
+    {
+        public static void Server()
+        {
+            IPEndPoint localEP = new IPEndPoint(IPAddress.Any, 0);
+            
+            UdpClient udp = new UdpClient(12345);
+
+            Console.WriteLine("Сервер ожидает сообщение от клиента...");
+
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = udp.Receive(ref localEP);
+                    string str1 = Encoding.UTF8.GetString(buffer);
+
+                    Massage? somemassage = Massage.FromJson(str1);
+                    if (somemassage != null)
+                    {
+                        Console.WriteLine(somemassage.ToString());
+                        Massage newmassage = new Massage("server", "Сообщение получено");
+                        string json = newmassage.ToJson();
+                        byte[] bytes = Encoding.UTF8.GetBytes(json);
+                        udp.Send(bytes, localEP);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректное сообщение!");
+                    }
+                }
+                catch(Exception e)
+                { 
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public static void Client(string nik)
+        {
+            IPEndPoint localEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            UdpClient udp = new UdpClient();
+
+            while (true)
+            {
+                Console.WriteLine("Введите сообщение:");
+                string text  = Console.ReadLine();
+                if (String.IsNullOrEmpty(text))
+                {
+                    break;
+                }
+                Massage newmassage = new Massage(nik, text);
+                string json = newmassage.ToJson();
+                byte[] bytes = Encoding.UTF8.GetBytes(json);
+                udp.Send(bytes,localEP);
+
+                byte[] buffer = udp.Receive(ref localEP);
+                string str1 = Encoding.UTF8.GetString(buffer);
+                Massage? somemassage = Massage.FromJson(str1);
+                Console.WriteLine(somemassage);
+
+            }
+        }
+    }
+}
---
+++ Разработка сетевого приложения/Урок 1. Работа с сетью чтение и запись данных в сеть. Клиентские и серверные приложения/Lcs1/Lcs1/Massage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Lcs1
+{
+    internal class Massage
+    {
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Stime { get; set; }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public static Massage? FromJson(string somemassage)
+        {
+            return JsonSerializer.Deserialize<Massage>(somemassage);
+        }
+
+        public Massage(string nikname, string text)
+        {
+            this.Name = nikname;
+            this.Text = text;
+            this.Stime = DateTime.Now;
+        }
+
+        public Massage() { }
+
+        public override string ToString()
+        {
+            return $"Получено сообщение от {Name} ({Stime.ToShortTimeString()}): \n {Text}";
+        }
+    }
+}
