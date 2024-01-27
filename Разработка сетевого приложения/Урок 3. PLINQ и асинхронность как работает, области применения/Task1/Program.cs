94c874284380b76ec4f374c0100f7fa9d1fb6d23
Matvey <matveypashnev6889@gmail.com>
2024-01-27 18:03:05 +0400

    Семинар 3

---
+++ Разработка сетевого приложения/Урок 3. PLINQ и асинхронность как работает, области применения/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task1
+/*Напише приложение для одновременного выполнения двух задач в потоках созданных с помощью Task.
+ *Нужно подсчитать сумму элементов каждого из массивов а потом сложить эти суммы полученные после выполнения каждого из потоков и вывести результат на экран.*/
+{
+    internal class Program
+    {
+        static int[] arr1 = Enumerable.Range(0, 100000).Select(x => Random.Shared.Next(0, 10)).ToArray();
+        static int[] arr2 = Enumerable.Range(0, 100000).Select(x => Random.Shared.Next(0, 10)).ToArray();
+
+        static async Task<int> Summ(int[] arr)
+        {
+            await Task.CompletedTask;
+
+            int sum = 0;
+            
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+
+        }
+
+        static async Task Main(string[] args)
+        {
+            var s1 = await Summ(arr1);
+            var s2 = await Summ(arr2);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine($"summ = {s1+s2}");
+        }
+    }
+}
